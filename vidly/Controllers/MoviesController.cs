6be2d6b36b2fe82dea1b950e7373542703d6de5b
Steven Hardy <steverhardy@gmail.com>
2019-07-30 09:51:19 -0400

    Routing lecture update

--- vidly/Controllers/MoviesController.cs
+++ vidly/Controllers/MoviesController.cs
@@ -17,7 +17,8 @@ namespace vidly.Controllers
             return View(movie);
         }
 
-        public ActionResult ByReleaseDate(int year, int month)
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
         {
             return Content(year + "/" + month);
         }
